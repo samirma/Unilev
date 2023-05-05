8b23939b3919c6fdc64f863e7923148fe8b38cca
maxper4 <maxenceperion@gmail.com>
2023-05-05 22:14:41 +0200

    Metadatas for NFTs of positions

--- src/Positions.sol
+++ src/Positions.sol
@@ -3,7 +3,9 @@ pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@solmate/tokens/ERC20.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@uniswapV3/contracts/interfaces/IUniswapV3Pool.sol";
 
 contract Positions is ERC721, Ownable {
@@ -17,7 +19,7 @@ contract Positions is ERC721, Ownable {
 
     struct PositionParams {
         IUniswapV3Pool v3Pool; // pool to trade
-        IERC20 token; // token to trade => should be token0 or token1 of v3Pool
+        ERC20 token; // token to trade => should be token0 or token1 of v3Pool
         uint256 value; // amount of token to trade
         bool isShort; // true if short, false if long
         uint8 leverage; // leverage of position => 0 if no leverage
@@ -31,10 +33,13 @@ contract Positions is ERC721, Ownable {
     uint256 public posId;
     mapping(uint256 => PositionParams) public positions;
 
+    string private constant BASE_SVG = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
     // Errors
 
     error PositionNotOpen(uint256 position);
     error NotOwnerOfPosition(uint256 position);
+    error PositionDoesNotExist(uint256 position);
 
     constructor(address _market) ERC721("Uniswap-MAX", "UNIMAX") {
         transferOwnership(_market);
@@ -51,6 +56,57 @@ contract Positions is ERC721, Ownable {
 
     function safeBurn(uint256 _posId) private {
         _burn(_posId);
+    }
+
+    function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
+        if(_tokenId >= posId) {
+            revert PositionDoesNotExist(_tokenId);
+        }
+
+        string memory json = Base64.encode(
+            bytes(
+            string.concat(tokenURIIntro(_tokenId),
+                tokenURIAttributes(positions[_tokenId])
+            ))
+        );
+
+        return string.concat('data:application/json;base64,', json);
+    }
+
+    function tokenURIIntro(uint256 _tokenId) private pure returns (string memory) {
+        return
+            string.concat(
+                '{"name": "Uniswap-Max Position #',
+                Strings.toString(_tokenId),
+                '", "description": "This NFT represent a position on Uniswap-Max. The owner can close or edit the position.", "image": "',
+                imageURI(_tokenId)
+        );
+    }
+
+    function tokenURIAttributes(PositionParams memory _position) private view returns (string memory) {
+        string[2] memory parts = [   // To avoid stack too deep error
+            string.concat(
+                '", "attributes": [ { "trait_type": "Token", "value": "', _position.token.name(),
+                    '"}, { "trait_type": "Amount", "value": "', Strings.toString(_position.value),
+                    '"} , { "trait_type": "Direction", "value": "', _position.isShort ? "Short" : "Long",
+                    '"}, { "trait_type": "Leverage", "value": "', Strings.toString(_position.leverage)
+            ),
+            string.concat(
+                '"}, { "trait_type": "Limit Price", "value": "', Strings.toString(_position.limitPrice),
+                '"}, { "trait_type": "Stop Loss Price", "value": "', Strings.toString(_position.stopLossPrice),
+                '"}, { "trait_type": "Status", "value": "', _position.status == PositionStatus.OPEN ? "Open" : 
+                    _position.status == PositionStatus.CLOSED ? "Closed" : "Liquidated",
+                '"}]}'
+            )
+        ];
+
+        return string.concat(parts[0], parts[1]);
+    }
+
+    function imageURI(uint256 _tokenId) private pure returns (string memory) {
+        string memory svg = string.concat(BASE_SVG, "UNISWAP-MAX #", Strings.toString(_tokenId), '</text></svg>');
+        
+        return string.concat('data:image/svg+xml;base64,', Base64.encode(bytes(svg)));
     }
 
     // --------------- Trader Zone ---------------
@@ -70,7 +126,7 @@ contract Positions is ERC721, Ownable {
         uint256 _posId = safeMint(_trader);
         positions[_posId] = PositionParams(
             IUniswapV3Pool(_v3Pool),
-            IERC20(_token),
+            ERC20(_token),
             _value,
             _isShort,
             _leverage,
@@ -83,6 +139,10 @@ contract Positions is ERC721, Ownable {
 
     function closePosition(uint256 _posId, address _trader) external onlyOwner {
         // TODO : apply rewards
+
+        if(_posId >= posId) {
+            revert PositionDoesNotExist(_posId);
+        }
 
         if(ownerOf(_posId) != _trader) {
             revert NotOwnerOfPosition(_posId);
@@ -126,6 +186,10 @@ contract Positions is ERC721, Ownable {
         uint256 _newLstopLossPrice,
         address _trader
     ) external onlyOwner {
+        if(_posId >= posId) {
+            revert PositionDoesNotExist(_posId);
+        }
+
         if(ownerOf(_posId) != _trader) {
             revert NotOwnerOfPosition(_posId);
         }
@@ -144,6 +208,10 @@ contract Positions is ERC721, Ownable {
         // TODO : check if liquidable
         // TODO : send liquidation reward
 
+        if(_posId >= posId) {
+            revert PositionDoesNotExist(_posId);
+        }
+
         if(positions[_posId].status != PositionStatus.OPEN) {
             revert PositionNotOpen(_posId);
         }
@@ -154,6 +222,10 @@ contract Positions is ERC721, Ownable {
     }
 
     function isLiquidable(uint256 _posId) public view returns (bool) {
+        if(_posId >= posId) {
+            return false;
+        }
+
         // TODO
         return positions[_posId].status == PositionStatus.OPEN && false;
     }
