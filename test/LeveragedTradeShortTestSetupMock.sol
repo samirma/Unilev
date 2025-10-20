84ff5a9cd891ea4da6aca4b22da14ca6f2daa433
Samir Antonio <Samir.MA.Antonio@ctw.bmwgroup.com>
2025-10-20 19:51:39 +0100

    Refactor leveraged trade tests and mock setups
    - Removed multiple test functions from LeveragedTradeShort.t.sol and SimpleTradeShort.sol to streamline the test suite.
    - Introduced LeveragedTradeShortTestSetupMock.sol to encapsulate common setup logic for leveraged trade tests.
    - Added MockSwapRouter.sol and updated MockV3Aggregator.sol to simplify mocking behavior in tests.
    - Created TestSetupMock.sol to provide a comprehensive testing environment with necessary contract deployments and configurations.
    - Ensured that the test suite remains functional while improving readability and maintainability.

--- test/LeveragedTradeShortTestSetupMock.sol
+++ test/LeveragedTradeShortTestSetupMock.sol
@@ -2,33 +2,38 @@
 pragma solidity ^0.8.0;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./utils/TestSetup.sol";
+import "./utils/TestSetupMock.sol";
 
-contract LeveragedTradeShort is TestSetup {
-    function test__leveragedTradeToCloseShort1() public {
+contract LeveragedTradeShort is TestSetupMock {
+    
+function test__leveragedTradeWithoutStopLossAndLiquidateShort() public {
         uint128 amount = 1000e6;
         uint24 fee = 3000;
         writeTokenBalance(alice, conf.addUsdc, amount);
 
-        assertEq(amount, IERC20(conf.addUsdc).balanceOf(alice));
-        assertEq(0, IERC20(conf.addUsdc).balanceOf(address(positions)));
+        //Mock intial values
 
         vm.startPrank(alice);
         IERC20(conf.addUsdc).approve(address(positions), amount);
         market.openPosition(conf.addUsdc, conf.addWbtc, uint24(fee), true, 2, amount, 0, 0);
-
-        assertEq(0, IERC20(conf.addUsdc).balanceOf(alice));
         assertApproxEqRel(amount * 3, IERC20(conf.addUsdc).balanceOf(address(positions)), 0.05e18);
 
-        assertEq(1, positions.totalNbPos());
+        vm.stopPrank();
+        // Theoretically badDebt = 45000e6 lets set the price to 44000e6
+        //Mock values
+
         uint256[] memory posAlice = positions.getTraderPositions(alice);
         assertEq(1, posAlice[0]);
         assertEq(alice, positions.ownerOf(posAlice[0]));
 
-        market.closePosition(posAlice[0]);
+        vm.startPrank(bob);
+        market.liquidatePosition(posAlice[0]);
 
-        assertApproxEqRel(amount, IERC20(conf.addUsdc).balanceOf(alice), 0.05e18);
+        console.log("balance of alice addUSDC ", IERC20(conf.addUsdc).balanceOf(alice));
+        // assertApproxEqRel(aaa, ERC20(conf.addWBTC).balanceOf(alice), 0.05e18); // TODO
+        assertEq(30003000, IERC20(conf.addUsdc).balanceOf(bob));
         assertEq(0, IERC20(conf.addUsdc).balanceOf(address(positions)));
+        assertEq(0, IERC20(conf.addWbtc).balanceOf(address(positions)));
     }
-
+   
 }
