07184ec2864fcbcca5a9fbf988ade2c0d649b924
Samir Antonio <Samir.MA.Antonio@ctw.bmwgroup.com>
2025-11-08 18:53:59 +0000

    fix: set BORROW_FEE to 0 in Positions contract and update leveraged trade test for WETH and WBTC balances

--- test/LeveragedTradeLongMock.t.sol
+++ test/LeveragedTradeLongMock.t.sol
@@ -8,46 +8,63 @@ contract LeveragedTradeLongMock is TestSetupMock {
 
 
 function test__leveragedTradeToCloseLong1() public {
-    uint128 amount = 1e8;
-    uint24 fee = 3000;
-    
-    writeTokenBalance(alice, conf.addWbtc, amount);
+        uint128 amount = 1e18;
+        uint24 fee = 3000;
+        writeTokenBalance(alice, conf.addWeth, amount);
 
-    assertEq(amount, IERC20(conf.addWbtc).balanceOf(alice));
-    assertEq(0, IERC20(conf.addWbtc).balanceOf(address(positions)));
+        // Assert initial balances before opening position
+        assertEq(amount, IERC20(conf.addWeth).balanceOf(alice));
+        assertEq(0, IERC20(conf.addWeth).balanceOf(address(positions)));
+        
+        vm.startPrank(deployer);
+        mockV3AggregatorEthUsd.updateAnswer(4000 * 1e8); // $4000
+        mockV3AggregatorWbtcUsd.updateAnswer(100000 * 1e8); // $100000
+        vm.stopPrank();
 
-    // Log USDC balance in lbPoolUsdc BEFORE open
-    uint256 usdcBalanceBefore = IERC20(conf.addUsdc).balanceOf(address(lbPoolUsdc));
-    console.log("USDC balance in lbPoolUsdc BEFORE open: ", usdcBalanceBefore);
+        vm.startPrank(alice);
+        // Approve the Positions contract to spend the input token (USDC)
+        IERC20(conf.addWeth).approve(address(positions), amount);
+        market.openPosition(conf.addWeth, conf.addWbtc, uint24(fee), true, 1, amount, 0, 0);
+        
+        assertApproxEqRel(amount * 2, IERC20(conf.addWeth).balanceOf(address(positions)), 0.5e17);
+        assertApproxEqRel(0, IERC20(conf.addWbtc).balanceOf(address(positions)), 0.5e17);
 
-    vm.startPrank(alice);
-    IERC20(conf.addWbtc).approve(address(positions), amount);
-    console.log("Open position");
-    market.openPosition(conf.addWbtc, conf.addUsdc, uint24(fee), false, 2, amount, 0, 0);
+        vm.stopPrank();
 
-    assertEq(0, IERC20(conf.addWbtc).balanceOf(alice));
-    assertApproxEqRel(amount * 2, IERC20(conf.addWbtc).balanceOf(address(positions)), 0.05e18);
+        vm.startPrank(deployer);
+        mockV3AggregatorEthUsd.updateAnswer(5000 * 1e8);
+        vm.stopPrank();
 
-    uint256 usdcBalanceAfter = IERC20(conf.addUsdc).balanceOf(address(lbPoolUsdc));
-    uint256 price = priceFeedL1.getPairLatestPrice(conf.addWbtc, conf.addUsdc);
-    uint256 totalBorrow = (amount * 1 * price) / (10**18);
-    uint256 openingFees = (totalBorrow * positions.BORROW_FEE()) / 10000;
-    assertApproxEqRel(usdcBalanceBefore + openingFees, usdcBalanceAfter, 0.05e18);
+        assertEq(1, positions.totalNbPos());
+        uint256[] memory posAlice = positions.getTraderPositions(alice);
 
-    assertEq(1, positions.totalNbPos());
-    uint256[] memory posAlice = positions.getTraderPositions(alice);
-    
-    assertEq(1, posAlice[0]);
-    assertEq(alice, positions.ownerOf(posAlice[0]));
-    console.log("Close position");
-    market.closePosition(posAlice[0]);
+        assertEq(1, posAlice[0]);
+        assertEq(alice, positions.ownerOf(posAlice[0]));
 
-    assertApproxEqRel(amount, IERC20(conf.addWbtc).balanceOf(alice), 0.05e18);
-    assertEq(0, IERC20(conf.addWbtc).balanceOf(address(positions)));
+        uint256 lbPoolWethBeforeClose = IERC20(conf.addWeth).balanceOf(address(lbPoolWeth));
+        console.log("WETH balance in lbPoolUsdc BEFORE close: ", lbPoolWethBeforeClose);
 
-    uint256 usdcBalanceAfterClose = IERC20(conf.addUsdc).balanceOf(address(lbPoolUsdc));
-    //assertGe(usdcBalanceBefore, usdcBalanceAfter);
-    console.log("USDC balance in lbPoolUsdc AFTER close: ", usdcBalanceAfterClose);
+        uint256 lbPoolWBTCBeforeClose = IERC20(conf.addWbtc).balanceOf(address(lbPoolWbtc));
+        console.log("WBTC balance in lbPoolUsdc BEFORE close: ", lbPoolWBTCBeforeClose);
+
+        console.log("Close position");
+        vm.startPrank(alice);
+        market.closePosition(posAlice[0]);
+        vm.stopPrank();
+
+        assertApproxEqRel(0, IERC20(conf.addWeth).balanceOf(address(positions)), 0.5e17);
+        assertApproxEqRel(0, IERC20(conf.addWbtc).balanceOf(address(positions)), 0.5e17);
+
+        uint256 wethBalanceAfterClose = IERC20(conf.addWeth).balanceOf(address(alice));
+        assertApproxEqRel(1193e15, wethBalanceAfterClose, 0.5e17);
+        uint256 wbtcBalanceAfterClose = IERC20(conf.addWbtc).balanceOf(address(alice));
+        assertApproxEqRel(0, wbtcBalanceAfterClose, 0.5e17);
+
+        uint256 lbPoolWethAfterClose = IERC20(conf.addWeth).balanceOf(address(lbPoolWeth));
+        console.log("WETH balance in lbPoolUsdc After close: ", lbPoolWethAfterClose);
+
+        uint256 lbPoolWBTCAfterClose = IERC20(conf.addWbtc).balanceOf(address(lbPoolWbtc));
+        console.log("WBTC balance in lbPoolUsdc After close: ", lbPoolWBTCAfterClose);
 
 }
 
