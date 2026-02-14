a8817a9086c8e029290b43ba6a8a60abe6c471cf
Samir Moreira Antonio <samir.antonio.sp@lufthansa-group.com>
2026-02-14 17:36:12 +0000

    refactor: differentiate network configurations for Ethereum and Polygon mainnets, adding Polygon-specific Chainlink and token addresses.

--- scripts/HelperConfig.sol
+++ scripts/HelperConfig.sol
@@ -22,8 +22,8 @@ contract HelperConfig {
     mapping(uint256 => NetworkConfig) public chainIdToNetworkConfig;
 
     constructor() {
-        chainIdToNetworkConfig[1] = getMainnetForkConfig();
-        chainIdToNetworkConfig[31337] = getMainnetForkConfig();
+        chainIdToNetworkConfig[1] = getMainnetEthereumConfig();
+        chainIdToNetworkConfig[137] = getMainnetPolygonConfig();
         activeNetworkConfig = chainIdToNetworkConfig[block.chainid];
     }
 
@@ -31,15 +31,19 @@ contract HelperConfig {
         return activeNetworkConfig;
     }
 
-    function getMainnetForkConfig()
+    function getMainnetEthereumConfig()
         internal
         pure
         returns (NetworkConfig memory mainnetNetworkConfig)
     {
         mainnetNetworkConfig = NetworkConfig({
+            // https://data.chain.link/feeds/ethereum/mainnet/eth-usd
             priceFeedEthUsd: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419, // ETH/USD Mainnet
+            // https://data.chain.link/feeds/ethereum/mainnet/btc-usd
             priceFeedWbtcUsd: 0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c, // BTC/USD Mainnet
+            // https://data.chain.link/feeds/ethereum/mainnet/usdc-usd
             priceFeedUsdcUsd: 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6, // USDC/USD Mainnet
+            // https://data.chain.link/feeds/ethereum/mainnet/dai-usd
             priceFeedDaiUsd: 0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9, // DAI/USD Mainnet
             nonfungiblePositionManager: 0xC36442b4a4522E871399CD717aBDD847Ab11FE88,
             swapRouter: 0xE592427A0AEce92De3Edee1F18E0157C05861564,
@@ -48,6 +52,31 @@ contract HelperConfig {
             addWeth: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
             addUsdc: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
             addDai: 0x6B175474E89094C44Da98b954EedeAC495271d0F,
+            treasure: 0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199
+        });
+    }
+
+    function getMainnetPolygonConfig()
+        internal
+        pure
+        returns (NetworkConfig memory mainnetNetworkConfig)
+    {
+        mainnetNetworkConfig = NetworkConfig({
+            // https://data.chain.link/feeds/polygon/mainnet/eth-usd
+            priceFeedEthUsd: 0xF9680D99D6C9589e2a93a78A04A279e509205945, // ETH/USD Polygon
+            // https://data.chain.link/feeds/polygon/mainnet/wbtc-usd
+            priceFeedWbtcUsd: 0xDE31F8bFBD8c84b5360CFACCa3539B938dd78ae6, // BTC/USD Polygon
+            // https://data.chain.link/feeds/polygon/mainnet/usdc-usd
+            priceFeedUsdcUsd: 0xfE4A8cc5b5B2366C1B58Bea3858e81843581b2F7, // USDC/USD Polygon
+            // https://data.chain.link/feeds/polygon/mainnet/dai-usd
+            priceFeedDaiUsd: 0x4746DeC9e833A82EC7C2C1356372CcF2cfcD2F3D, // DAI/USD Polygon
+            nonfungiblePositionManager: 0xC36442b4a4522E871399CD717aBDD847Ab11FE88,
+            swapRouter: 0xE592427A0AEce92De3Edee1F18E0157C05861564,
+            liquidityPoolFactoryUniswapV3: 0x1F98431c8aD98523631AE4a59f267346ea31F984,
+            addWbtc: 0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6,
+            addWeth: 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619,
+            addUsdc: 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174,
+            addDai: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
             treasure: 0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199
         });
     }
